4758d1c6e1d74f32373cb9a8fdfc486ecaf39031
betse <betse11001@gmail.com>
2025-10-06 01:05:55 +0200

    fix

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -1,4 +1,4 @@
-output "public_ip" {
-  description = "VM public IP"
-  value       = google_compute_instance.vm.network_interface[0].access_config[0].nat_ip
+output "instance_public_ip" {
+  description = "The public IP address of the VM instance."
+  value       = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
 }
